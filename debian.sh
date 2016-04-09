efd9827cc13f2877e669b7230f6b1b6fb7f173b9
Neil Galvin <neil@galv.in>
2016-04-09 21:59:55 +0100

    Fixed bash variable expansion and dir checks before deletion

--- debian.sh
+++ debian.sh
@@ -3,12 +3,17 @@ echo "Patching release"
 VERSION=${DOWNLOAD%"/"$FILENAME}
 VERSION=${VERSION##*/}
 FOLDER=${FILENAME%.*}
-dpkg -x "${DOWNLOADDIR}/${FILENAME}" "${DOWNLOADDIR}/${FOLDER}"
-tar xvfj  "${SCRIPTPATH}/amd64.tar.bz2 -C ${DOWNLOADDIR}"
-mv "${DOWNLOADDIR}/${FOLDER}/usr" "${DOWNLOADDIR}/amd64/debian"
-sed -i "s#{{version}}#${VERSION}#" "${DOWNLOADDIR}/amd64/debian/DEBIAN/control"
-sudo dpkg-deb --build "${DOWNLOADDIR}/amd64/debian"
-sudo dpkg -i "${DOWNLOADDIR}/amd64/debian.deb"
+dpkg -x "${DOWNLOADDIR}"/"${FILENAME}" "${DOWNLOADDIR}"/"${FOLDER}"
+tar xvfj  "${SCRIPTPATH}/"amd64.tar.bz2 --directory "${DOWNLOADDIR}"
+mv "${DOWNLOADDIR}"/${FOLDER}/usr "${DOWNLOADDIR}"/amd64/debian
+sed -i "s#{{version}}#${VERSION}#" "${DOWNLOADDIR}"/amd64/debian/DEBIAN/control
+sudo dpkg-deb --build "${DOWNLOADDIR}"/amd64/debian
+sudo dpkg -i "${DOWNLOADDIR}"/amd64/debian.deb
 echo "Removing build files"
-rm -rf "${DOWNLOADDIR}/amd64"
-rm -rf "${DOWNLOADDIR}/${FOLDER}"
+if [ -d "${DOWNLOADDIR}"/amd64 ]; then
+        rm -rf "${DOWNLOADDIR}"/amd64
+fi
+
+if [ -d "${DOWNLOADDIR}"/${FOLDER} ]; then
+        rm -rf "${DOWNLOADDIR}"/${FOLDER}
+fi
